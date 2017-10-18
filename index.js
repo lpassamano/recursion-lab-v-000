933b68f04a87561cd4639f5d26c909d50a41e026
lpassamano <leighayna.josele@gmail.com>
2017-10-17 22:41:44 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 // Code your solution here!
 
+function printString(myString) {
+  // prints out all characters of a passed in string
+  console.log(myString[0]);
+
+  if (myString.length > 1) {
+    myString = myString.substring(1, myString.length);
+    printString(myString);
+  } else {
+    return true;
+  }
+}
+
+function reverseString(string) {
+  if (string.length > 1) {
+    var lastLetter = string.length - 1;
+    return string[lastLetter] + reverseString(string.substring(0, lastLetter));
+  } else if (string.length == 1); {
+    return string[0];
+  }
+}
